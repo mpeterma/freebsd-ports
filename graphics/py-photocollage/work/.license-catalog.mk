87f2e2185cdd8f069e588984a9fe7afe2be099ad
Matthias Petermann <mpeterma@ws1.local>
2015-05-15 08:31:49 +0200

    Removed working directory

--- graphics/py-photocollage/work/.license-catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=GPLv2
-_LICENSE_NAME=GNU General Public License version 2
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF GPL OSI
-_LICENSE_DISTFILES=adrienverge-PhotoCollage-1.3.0.a.20150514-98f9b4f_GH0.tar.gz
